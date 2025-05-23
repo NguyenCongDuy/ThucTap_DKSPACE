0bed2472a0b8e1dfbe740c57a4b2b0694fe15310
NguyenCongDuy <146914945+NguyenCongDuy@users.noreply.github.com>
2025-05-23 12:03:07 +0700

    Câu lệnh thêm dữ liệu mẫu của ngày 4

--- MySQL/Ngay04/LenhTaoBang.sql
+++ MySQL/Ngay04/LenhTaoBang.sql
@@ -37,3 +37,24 @@ CREATE TABLE Enrollments (
 
 -- xóa cơ sở dữ liệu nếu như không cần dùng nữa
 DROP DATABASE Mysql_OnlineLearning
+
+-- thêm dữ liệu vào bảng 
+
+INSERT INTO Students (full_name, email)
+VALUES 
+('Nguyễn Văn A', 'nguyenvana@gmail.com'),
+('Trần Thị B', 'tranthib@yahoo.com'),
+('Lê Văn C', 'levanc@gmail.com');
+
+INSERT INTO Courses (title, description, price)
+VALUES 
+('Lập trình PHP cơ bản', 'Khóa học dành cho người mới bắt đầu học PHP', 1000000),
+('MySQL nâng cao', 'Khóa học giúp làm chủ MySQL với ví dụ thực tế', 1500000),
+('HTML & CSS', 'Xây dựng giao diện website đẹp và chuẩn responsive', 500000);
+
+INSERT INTO Enrollments (student_id, course_id)
+VALUES 
+(1, 1), -- Nguyễn Văn A học Lập trình PHP cơ bản
+(2, 2), -- Trần Thị B học MySQL nâng cao
+(1, 3), -- Nguyễn Văn A học thêm HTML & CSS
+(3, 1); -- Lê Văn C học Lập trình PHP cơ bản
